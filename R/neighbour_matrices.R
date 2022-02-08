bae9ea5c07892630225719d75f07ffbebb2b68d1
Felix Hofmann <felix_hofmann_13@hotmail.com>
2022-02-08 15:46:27 +0100

    Add function to create neighbourhood order matrices

--- R/neighbour_matrices.R
+++ R/neighbour_matrices.R
@@ -1,37 +1,78 @@
-# get_nb_matrix <- function(geoms, age_groups, cache_dir = NULL){
-#
-#   # check input
-#   if(!(nuts_level %in% 0:3) || length(nuts_level) != 1) stop("Argument 'nuts_level' must be one of c(0, 1, 2, 3).")
-#   if(!(with_age %in% c(TRUE, FALSE)) || length(with_age) != 1) stop("Argument 'with_age' must be either TRUE or FALSE.")
-#   cache_dir <- get_cache_dir(cache_dir)
-#
-#   # Set parameters
-#   nb_pattern <- "F***1****" # Use "F***T****" if a touch point is enough for two regions to be nbs
-#
-#   # load level 3 regions and age groups
-#   file <- make_path(cache_dir, "case_regions_3.rds")
-#   if(!file.exists(file)){
-#     get_cases(cache_dir = cache_dir)
-#   }
-#   regions <- readRDS(file)
-#   file <- make_path(cache_dir, "agegroups.rds")
-#   if(!file.exists(file)){
-#     get_cases(cache_dir = cache_dir)
-#   }
-#   agegroups <- readRDS(file)
-#
-#
-#
-# }
+#' Compute neighbourhood order matrices stratified by region and/or age group
+#'
+#' @param by A \code{character} vector. Contains one or both of \code{c("age", "region")}.
+#' @param nuts_level A \code{character} vector of length 1 indicating the desired NUTS level of spatial regions. The argument is ignored if
+#' \code{by = "age"}.
+#' @template cache_dir
+#'
+#' @return A \code{matrix} containing the neighbourhood order of the units defined via the \code{by} argument.
+#' @export
+#'
+#' @examples
+#' nb_regions_agegroups <- get_nb_matrix(nuts_level = 1)
+#' nb_regions <- get_nb_matrix(by = "region", nuts_level = 1)
+#' nb_age <- get_nb_matrix(by = "age", nuts_level = 1)
+#'
+get_nb_matrix <- function(by = c("age", "region"), nuts_level = 3, cache_dir = NULL){
+
+
+  if(!(nuts_level %in% 0:3) || length(nuts_level) != 1) stop("Argument 'nuts_level' must be one of c(0, 1, 2, 3).")
+  by <- match.arg(by, several.ok = TRUE)
+  cache_dir <- get_cache_dir(cache_dir)
+
+  # Set parameters
+  nb_pattern <- "F***1****" # Use "F***T****" if a touch point is enough for two regions to be nbs
+  filename <- paste0("nb_order_mat_", nuts_level, ".rds")
+
+  # Check whether file exists
+  from_cache <- read_from_cache(cache_dir = cache_dir, filename = filename,
+                                cutoff = 90, units = "days")
+
+  # get pre-processed data from file or from source
+  if(from_cache){
+    dat <- readRDS(make_path(cache_dir, filename))
+  } else {
+    # load necessary data
+    geoms <- get_geoms()[[nuts_level + 1]]
+    if(file.exists(make_path(cache_dir, paste0("case_regions_", nuts_level, ".rds")))){
+      regions <- readRDS(make_path(cache_dir, paste0("case_regions_", nuts_level, ".rds")))
+    } else {
+      get_cases()
+      regions <- readRDS(make_path(cache_dir, paste0("case_regions_", nuts_level, ".rds")))
+    }
+    if(file.exists(make_path(cache_dir, "agegroups.rds"))){
+      agegroups <- readRDS(make_path(cache_dir, "agegroups.rds"))
+    } else {
+      get_cases()
+      regions <- readRDS(make_path(cache_dir, "agegroups.rds"))
+    }
+    # calculate matrix
+    dat <- compute_nb_matrix(regions = regions, agegroups = agegroups,
+                             geoms = geoms, nb_pattern = nb_pattern,
+                             save_location = make_path(cache_dir, filename))
+  }
+
+  # return matrix as desired
+  if(!(length(by) == 2L)){
+    if(by == "region"){
+      idx <- grepl(sub("^.+\\.(.+)$", "\\1", colnames(dat)[1]), colnames(dat))
+      dat <- dat[idx, idx]
+      colnames(dat) <- rownames(dat) <- gsub("^(.+)\\..+$", "\\1", colnames(dat))
+    } else if(by == "age") {
+      agegroups <- unique(gsub("^.+\\.(.+)$", "\\1", colnames(dat)))
+      dat <- dat[seq_along(agegroups), seq_along(agegroups)]
+      colnames(dat) <- rownames(dat) <- agegroups
+    } else {
+      stop("Check function. This should not be possible.")
+    }
+  }
+
+  return(dat)
+}
 
 # geoms <- get_geoms()[[4]]
 # agegroups <- readRDS(make_path(tempdir(), "agegroups.rds"))
 # regions <- readRDS(make_path(tempdir(), "case_regions_3.rds"))
-
-# This function only needs geoms and agegroups. The checking whether geoms
-# are actually correct is done somewhere else
-
-
 
 #' Compute neighbourhood order matrices stratified by region and age group
 #'
@@ -39,21 +80,34 @@
 #' @param agegroups A \code{character} vector containing all age group labels in increasing order.
 #' @param geoms An object of class \code{sf} with at least a column named \code{region} that contains the unit names and a geometry column.
 #' @param nb_pattern The neighbourhood \code{pattern} that is passed to \code{\link[sf]{st_relate}}.
-#' @param cache_dir The directory where the output matrix is saved.
+#' @param save_location A filesystem path where the output matrix is saved.
 #'
-#' @return
-#' @export
+#' @return A matrix containing the neighbourhood order between the units
 #'
-#' @examples
-compute_nb_matrix <- function(regions, agegroups, geoms, nb_pattern, cache_dir = NULL){
+#' @importFrom stats toeplitz
+#' @importFrom sf st_relate
+#' @importFrom parallel mclapply detectCores
+#' @importFrom tidyr expand_grid
+compute_nb_matrix <- function(regions, agegroups, geoms, nb_pattern, save_location = NULL){
 
-  # Check regions
+  ## check regions
   if(!all(regions %in% geoms$region)) stop("Not all regions are present in geoms.")
   if(!all(geoms$region %in% regions)) stop("Some geoms are not present on the passed regions.")
 
   # age groups
   ## age group matrix (relies on age groups being in correct order)
   age_mat <- stats::toeplitz(seq_along(agegroups) - 1L)
+
+  # avoid error in R CMD CHECK
+  # code taken from https://stackoverflow.com/questions/50571325/r-cran-check-fail-when-using-parallel-functions
+  chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
+  if (nzchar(chk) && chk == "TRUE") {
+    # use 2 cores in CRAN/Travis/AppVeyor
+    num_workers <- 2L
+  } else {
+    # use all cores in devtools::test()
+    num_workers <- parallel::detectCores() - 1
+  }
 
   # Compute the actual matrix
   nb_list <- sf::st_relate(geoms, pattern = nb_pattern)
@@ -73,7 +127,7 @@ compute_nb_matrix <- function(regions, agegroups, geoms, nb_pattern, cache_dir =
     # now replace each element of this vector by age_mat + vector_element
     test <- do.call("cbind", lapply(out, "+", y = age_mat))
   },
-  mc.cores = parallel::detectCores() - 1L,
+  mc.cores = num_workers,
   mc.preschedule = FALSE,
   mc.cleanup = TRUE))
 
@@ -83,7 +137,7 @@ compute_nb_matrix <- function(regions, agegroups, geoms, nb_pattern, cache_dir =
   colnames(combined_mat) <- rownames(combined_mat) <- paste0(nms$region, ".", nms$age)
 
   # save object
-  saveRDS(combined_mat, file = filename)
+  saveRDS(combined_mat, file = save_location)
 
   return(combined_mat)
 }
